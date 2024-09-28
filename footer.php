f2b058070089cfffc6f402e86f5e864f0c4ad118
William Parry <williamparry@gmail.com>
2024-09-28 20:44:17 +1000

    change footer to sit at the bottom

--- footer.php
+++ footer.php
@@ -10,40 +10,39 @@
  */
 
 ?>
-
-    <footer class="footer">
-        <div class="social-bar">
-            <a href="https://twitter.com/DadLAN_au" aria-label="Twitter" target="_blank">
-                <img class="social-icon" src="/wp-content/themes/dadlan/assets/icons/x-twitter.svg" alt="Twitter">
-            </a>
-            <a href="https://github.com/dadlan-au" aria-label="Github" target="_blank">
-                <img class="social-icon" src="/wp-content/themes/dadlan/assets/icons/github.svg" alt="Github">
-            </a>
-            <a href="https://www.linkedin.com/company/dadlan-au" aria-label="LinkedIn" target="_blank">
-                <img class="social-icon" src="/wp-content/themes/dadlan/assets/icons/linkedin.svg" alt="LinkedIn">
-            </a>
-            <a href="https://www.facebook.com/dadlanau" aria-label="Facebook" target="_blank">
-                <img class="social-icon" src="/wp-content/themes/dadlan/assets/icons/facebook.svg" alt="Facebook">
-            </a>
-            <a href="https://www.instagram.com/dadlan_aus" aria-label="Instagram" target="_blank">
-                <img class="social-icon" src="/wp-content/themes/dadlan/assets/icons/instagram.svg" alt="Instagram">
-            </a>
-            <a href="https://discord.gg/dadlan" aria-label="Discord" target="_blank">
-                <img class="social-icon" src="/wp-content/themes/dadlan/assets/icons/discord.svg" alt="Discord">
-            </a>
-        </div>
-
-        <br>
-
-        <div>
-            <nav>
-                <a href="/membership/">Membership</a> | <a href="https://www.volunteer.com.au/volunteering-organisations/31242/DadLAN-LTD">Volunteering Opportunities</a> | <a href="/resources/">Resources</a>
-            </nav>
-        </div>
-
-        <br> &copy; 2024 DadLAN Ltd | <a href="https://www.acnc.gov.au/charity/charities/b4327b23-c8e9-ee11-a203-002248122b18/profile">Registered Charity</a>
-    </footer>
 </div><!-- #page -->
+<footer class="footer">
+    <div class="social-bar">
+        <a href="https://twitter.com/DadLAN_au" aria-label="Twitter" target="_blank">
+            <img class="social-icon" src="/wp-content/themes/dadlan/assets/icons/x-twitter.svg" alt="Twitter">
+        </a>
+        <a href="https://github.com/dadlan-au" aria-label="Github" target="_blank">
+            <img class="social-icon" src="/wp-content/themes/dadlan/assets/icons/github.svg" alt="Github">
+        </a>
+        <a href="https://www.linkedin.com/company/dadlan-au" aria-label="LinkedIn" target="_blank">
+            <img class="social-icon" src="/wp-content/themes/dadlan/assets/icons/linkedin.svg" alt="LinkedIn">
+        </a>
+        <a href="https://www.facebook.com/dadlanau" aria-label="Facebook" target="_blank">
+            <img class="social-icon" src="/wp-content/themes/dadlan/assets/icons/facebook.svg" alt="Facebook">
+        </a>
+        <a href="https://www.instagram.com/dadlan_aus" aria-label="Instagram" target="_blank">
+            <img class="social-icon" src="/wp-content/themes/dadlan/assets/icons/instagram.svg" alt="Instagram">
+        </a>
+        <a href="https://discord.gg/dadlan" aria-label="Discord" target="_blank">
+            <img class="social-icon" src="/wp-content/themes/dadlan/assets/icons/discord.svg" alt="Discord">
+        </a>
+    </div>
+
+    <br>
+
+    <div>
+        <nav>
+            <a href="/membership/">Membership</a> | <a href="https://www.volunteer.com.au/volunteering-organisations/31242/DadLAN-LTD">Volunteering Opportunities</a> | <a href="/resources/">Resources</a>
+        </nav>
+    </div>
+
+    <br> &copy; 2024 DadLAN Ltd | <a href="https://www.acnc.gov.au/charity/charities/b4327b23-c8e9-ee11-a203-002248122b18/profile">Registered Charity</a>
+</footer>
 
 <?php wp_footer(); ?>
 
